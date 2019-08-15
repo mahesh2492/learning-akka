4c790b8a4c6d0e9bbead3974f7e95de4435475fa
Mahesh Chand <mahesh.kndpl@gmail.com>
2019-08-15 16:26:38 +0530

    added poison pill message actor example

---
+++ src/main/scala/akka/examples/stopping/actors/PoisonedActor.scala
@@ -0,0 +1,30 @@
+package akka.examples.stopping.actors
+
+import akka.actor.{Actor, ActorSystem, PoisonPill, Props}
+
+class PoisonedActor extends Actor {
+
+  override def receive: Receive = {
+    case message: String => println(s"Message has been received: $message")
+    case _               => println("Gpt the unknown message!")
+  }
+
+  override def postStop: Unit = println("TestActor::postStop called")
+}
+
+object PoisonPillActor extends App {
+  val system = ActorSystem("PoisonedActor")
+  val poisonActor = system.actorOf(Props[PoisonedActor], name = "PoisonPillActor")
+
+  poisonActor ! "before poison pill"
+
+
+  //poison pill
+  poisonActor ! PoisonPill
+
+  //the messages will never be processed
+  poisonActor ! "after poison pill"
+  poisonActor ! "Hello"
+
+  system.terminate()
+}
---
+++ src/main/scala/akka/examples/stopping/actors/StopActor.scala
@@ -0,0 +1,22 @@
+package akka.examples.stopping.actors
+
+import akka.actor.{Actor, ActorSystem, Props}
+
+class StopActor extends Actor {
+
+  override def receive: Receive = {
+    case _ => println("Message has been received")
+  }
+}
+
+object KillActor extends App {
+
+  val system = ActorSystem("StoppingActor")
+  val actor = system.actorOf(Props[StopActor], name = "KillActor")
+
+  actor ! "hello"
+
+  //We can kill this method to stop the actor
+  system.stop(actor)
+  system.terminate()
+}
