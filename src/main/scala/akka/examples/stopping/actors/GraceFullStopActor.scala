3f6fef98864e5bd17e4588bc0e4d083f300af04c
Mahesh Chand <mahesh.kndpl@gmail.com>
2019-08-15 16:36:20 +0530

    added gracefulstop actor example

---
+++ src/main/scala/akka/examples/stopping/actors/GraceFullStopActor.scala
@@ -0,0 +1,33 @@
+package akka.examples.stopping.actors
+
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.pattern.gracefulStop
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class GraceFullStopActor extends Actor {
+
+  override def receive: Receive = {
+    case message: String => println(s"Message has been receivec: $message")
+    case _               => println("Got unknown message")
+  }
+
+  override def postStop(): Unit = println("GraceFullStopActor: postStop has been called")
+}
+
+object GraceFullStopTest extends App {
+  val system = ActorSystem("GraceFullStopActor")
+  val graceFullStopActor = system.actorOf(Props[GraceFullStopActor], name = "GraceFullStopTest")
+
+  graceFullStopActor ! "Going to kill you"
+  try {
+    val stopped = gracefulStop(graceFullStopActor, 2 seconds)
+    Await.result(stopped, 3 seconds)
+    println("testActor was stopped")
+  } catch {
+    case e: Exception => e.printStackTrace()
+  } finally {
+    system.terminate()
+  }
+}
